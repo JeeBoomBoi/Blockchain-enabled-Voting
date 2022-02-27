6948980d4fda98dd5b908bc991339dbac566e948
JeeBoomBoi <jeeboxical@gmail.com>
2022-02-27 13:56:09 +0530

    Poll component logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showForm = false;
+
+  polls = [{
+    question: 'Do you like dogs or cats?',
+    image: "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    votes: [0,5,7],
+    voted: true
+  },
+  {
+    question: 'Best month for holidays?',
+    image: "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    votes: [1,6,4],
+    voted: true
+  }
+]
 }
